13b82f4bbcfb0629c0b1d12594f7110f9971173c
Apokochito <dpinto1@ucol.mx>
2018-11-02 09:50:38 -0600

    First steps, React

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+import './App.css';
+
+class App extends Component {
+   
+  state = {
+    counters: [
+    {id:1, value:4},
+    {id:2, value:0},
+    {id:3, value:0},
+    {id:4, value:0}
+    ]
+  };
+
+  // Review about Constructor, Redered, Mounted, Updated and Unmounted from virtual to real DOM.
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+
+  render() {
+    return (
+      <React.Fragment>
+      <NavBar 
+      totalCounters={this.state.counters.filter(c => c.value > 0).length}
+      />
+      <main className="container">
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+      />
+      </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
